10f34e9e479844d2940139076336a21709057ea4
kong-fan-xing123 <kfyxb@outlook.com>
2021-01-12 13:11:51 +0800

    first commit

---
+++ classtools.py
@@ -0,0 +1,12 @@
+class AttrDisplay:
+ """
+ ...
+ """
+ def gatherAttrs(self):
+  attrs=[]
+  for key in sorted(self.__dict__):
+   attrs.append('%s=%s' % (key,getattr(self,key)))
+  return ', '.join(attrs)
+ def __str__(self):
+  return '[%s: %s]' % (self.__class__.__name__,self.gatherAttrs())
+ 
